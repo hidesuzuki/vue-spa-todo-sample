614d08b866d5db654657c22509269734f5b124ef
hidesuzuki <hide_suzuki@sharecrest.com>
2019-11-30 20:04:27 +0900

    sample

--- src/main.js
+++ src/main.js
@@ -3,6 +3,114 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+var STORAGE_KEY = 'todos-vuejs-2.0'
+var todoStrage = {
+  fetch: function(){
+    var todos = JSON.parse(localStrage.getItem(STORAGE_KEY) || '[]');
+    todos.foreach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStrage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos){
+    localStrage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
+var filters = {
+  all: function(todos) {
+    return todos;
+  },
+  active: function(todos) {
+    return todos.filter(function(todo){
+      return !todo.completed;
+    })
+  },
+  completed: function(todos){
+    return todos.filter(function(todo){
+      return todo.completed;
+    })
+  }
+};
+let app = new Vue({
+  data: {
+    todos: todoStrage.fetch(),
+    newToto: '',
+    editedTodo: null,
+    visibility: 'all'
+  },
+  watch:{
+    todos:{
+      handler: function(todos) {
+        todoStrage.save(todos);
+      },
+      deep: true
+    }
+  },
+  computed: {
+    filteredTodos: function(){
+      return filters[this.visibility](this.todos)
+    },
+    remaining: function() {
+      return filters.active(this.todos).length
+    },
+    allDone: {
+      get: function() {
+        return this.remaining === 0
+      },
+      set: function(value){
+        this.todos.forEach(function(todo){
+          todo.completed = value
+        })
+      }
+
+    }
+  },
+  filters:{
+    pluralize: function(n){
+      return n === 1? 'item' : 'items';
+    }
+  },
+  methods: {
+    addTodo: function () {
+      let value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        id: todoStrage.uid++,
+        title: value,
+        completed: false
+      });
+      this.newTodo = ''
+    },
+    removeTodo: function (todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1)
+    },
+    editTodo: function (todo) {
+      this.beforeEditCache = todo.title;
+      this.editedTodo = todo;
+    },
+    doneEdit: function (todo) {
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.title = todo.title.trim();
+      if (!todo.title) {
+        this.removeTodo(todo);
+      }
+    },
+    cancelEdit: function (todo) {
+      this.editesTodo = null;
+      todo.title = this.beforeEditCache;
+    },
+    removeCompleted: function(){
+      this.todos = filters.active(this.todos)
+    }
+  }
+
+});
+
+app.$mount('#app')
